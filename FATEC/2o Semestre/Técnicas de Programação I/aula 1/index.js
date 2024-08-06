809a7b764826b96dd5b9e99f9d5311d2ec887a32
frevisto <VITOR.ZONZINI@FATEC.SP.GOV.BR>
2024-08-06 16:49:30 -0300

    05-08

---
+++ FATEC/2o Semestre/Técnicas de Programação I/aula 1/index.js
@@ -0,0 +1,6 @@
+function inf_sum(a, b) {
+    return a + b;
+}
+;
+var res = inf_sum(2, 3);
+console.log(res);
---
+++ FATEC/2o Semestre/Técnicas de Programação I/aula 1/index.ts
@@ -0,0 +1,7 @@
+function inf_sum(a:number,b:number){
+    return a + b ;
+};
+
+var res:number = inf_sum(2,3);
+
+console.log(res);
