4b4aeeef50dda482886c47d348c16c1fd7ffed53
frevisto <vitor.zonzini@fatec.sp.gov.br>
2024-12-05 15:01:34 -0300

    add cont

--- FATEC/2o Semestre/Matemática Computacional/Exercicios/EP 5.7/src/Calc_Tng.ts
+++ FATEC/2o Semestre/Matemática Computacional/Exercicios/EP 5.7/src/Calc_Tng.ts
@@ -1,18 +1,29 @@
+
+
 function grausParaRadianos(graus: number): number {
-
     return graus * (Math.PI / 180);
-
 }
 
 export function calcularCatetoAdjacente(angulo: number, catetoOposto: number): void {
+
+
+    if (angulo <= 0 || angulo >= 90) {
+        console.error("Erro: O ângulo deve estar no intervalo 0°, 90°.");
+        return;
+    }
+
+    if (catetoOposto <= 0) {
+        console.error("Erro: O cateto oposto deve ser maior que zero.");
+        return;
+    }
 
     const anguloRadianos = grausParaRadianos(angulo);
 
     const catetoAdjacente = (catetoOposto / Math.tan(anguloRadianos)).toFixed(3);
 
-
-    console.log("VALOR de D:\n", catetoAdjacente,"METROS.")
+    console.log("VALOR de D:\n", catetoAdjacente, "METROS.");
 }
+
 
 
 
